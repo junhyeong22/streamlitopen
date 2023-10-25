57f2d1b411a40c91ff90bbc7e3b17b99486791ad
junhyeong22 <148923070+junhyeong22@users.noreply.github.com>
2023-10-25 23:01:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -84,9 +84,10 @@ st.text("-33.87 이하: 전구 교체 필요")
 st.sidebar.title('WE_ARE_ALL : 우리는 전부')
 
 col3,col4 = st.columns([2,3])
-image_url = "https://blogger.googleusercontent.com/img/a/AVvXsEhwH0dMFEoRwggB-g6Rfber_OaPbjcw3L--pR--LkztJNxwWdcPpnOxSOOXUiexAi7P9frycNcJ2VjHjErdfGthSYWFpsTj6UPjWMaEUcmasuFIjKK5EC6mJHRyjha16nywTpUqFKcPAtmcUfNG-F0Pd_EBP38Ww1d7csoH1CXlhT8-DgjRtW-mYGDoLLQ"
+
 with col3 : 
-    st.sidebar.image(image_url, width=40, caption=None)
+    image_url = "https://blogger.googleusercontent.com/img/a/AVvXsEhwH0dMFEoRwggB-g6Rfber_OaPbjcw3L--pR--LkztJNxwWdcPpnOxSOOXUiexAi7P9frycNcJ2VjHjErdfGthSYWFpsTj6UPjWMaEUcmasuFIjKK5EC6mJHRyjha16nywTpUqFKcPAtmcUfNG-F0Pd_EBP38Ww1d7csoH1CXlhT8-DgjRtW-mYGDoLLQ"
+    st.sidebar.image(image_url, width=30, caption=None)
 with col4 :
     st.sidebar.header("대구공공시설관리공단")
     
