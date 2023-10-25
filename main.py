6b8e79f231b5ba5e636d6e0d5be6071e071c4628
junhyeong22 <148923070+junhyeong22@users.noreply.github.com>
2023-10-25 22:46:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -76,12 +76,13 @@ col2.checkbox('절연저항이 불량한 지중선을 정비하고 노후 가로
 
 st.sidebar.title('WE_ARE_ALL : 우리는 전부')
 
-col1,col2 = st.columns([2,3])
+col1,col2 = st.columns([3,3])
 image_url = "https://blogger.googleusercontent.com/img/a/AVvXsEhwH0dMFEoRwggB-g6Rfber_OaPbjcw3L--pR--LkztJNxwWdcPpnOxSOOXUiexAi7P9frycNcJ2VjHjErdfGthSYWFpsTj6UPjWMaEUcmasuFIjKK5EC6mJHRyjha16nywTpUqFKcPAtmcUfNG-F0Pd_EBP38Ww1d7csoH1CXlhT8-DgjRtW-mYGDoLLQ"
 with col1 : 
     st.sidebar.image(image_url, width=50, caption=None)
 with col2 :
     st.sidebar.header("대구공공시설관리공단")
+    
 st.sidebar.header("")
 st.sidebar.header("<가로등 정비 솔루션 POC>")
 
